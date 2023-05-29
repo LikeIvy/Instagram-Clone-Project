ef09c5327a7aca141fb3cf4a94009abb58dda010
LikeIvy <ivy74115@gmail.com>
2023-05-30 00:16:06 +0900

    프로필 기능 수정

--- Instagram/user/urls.py
+++ Instagram/user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Join, Login, Logout
+from .views import Join, Login, Logout, Uploadprofile
 
 
 
 urlpatterns = [
     path('join', Join.as_view()), 
     path('login', Login.as_view()), 
-    path('logout', Logout.as_view()), 
+    path('logout', Logout.as_view()),
+    path('profile/upload', Uploadprofile.as_view()), 
 ]
--- Instagram/user/views.py
+++ Instagram/user/views.py
@@ -3,6 +3,9 @@ from rest_framework.views import APIView
 from .models import User
 from django.contrib.auth.hashers import make_password
 from rest_framework.response import Response
+from Instagram.settings import MEDIA_ROOT
+from uuid import uuid4
+import os
 
 # Create your views here.
 class Join(APIView):
@@ -54,3 +57,27 @@ class Logout(APIView):
     def get(self, request):
         request.session.flush()
         return render(request, 'user/login.html')
+    
+
+class Uploadprofile(APIView):
+    def post(self, request):
+        file = request.FILES['file'] 
+        email = request.data.get('email')
+
+        uuid_name = uuid4().hex 
+        save_path = os.path.join(MEDIA_ROOT, uuid_name) 
+
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+                
+        profile_image = uuid_name
+        
+        
+        user = User.objects.filter(email=email).first()
+
+        user.profile_img = profile_image
+
+        user.save()
+    
+        return Response(status=200)
